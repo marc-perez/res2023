8cf47454a346a58c655239dfaa0eeee732f2eff4
Marc P <33258321+marc-perez@users.noreply.github.com>
2023-04-29 23:40:17 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", () => {menu.classList.toggle("open");});
